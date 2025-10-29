e4c1f1c4660b9bb435c2b4a972fc4fbf334b160c
Anastasia <nastya-banova@yandex.ru>
2025-10-29 19:50:01 +0300

    new 2

--- src/pages/CalculatePage.jsx
+++ src/pages/CalculatePage.jsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 const CalculatePage = () => {
   const [num1, setNum1] = useState(5);
   const [num2, setNum2] = useState(5);
-  
+  const [result, setresult] = useState('');
+
   const plus = (a, b) => {
     return a + b;
   }
@@ -21,7 +22,54 @@ const CalculatePage = () => {
   }
 
   return (
-    <div>
+    <div className='container' style={{maxWidth: 600}}>
+      <h2>Калькулятор</h2>
+      <div className='card'>
+        <div className='card-body'>
+          <div className='row g-3 mb-3'>
+            <div className='col-md-6'>
+              <label className='form-label'>Число 1</label>
+              <input
+              type='number'
+              className='form-control'
+              value={num1}
+              onChange={(e) => setNum1(Number(e.target.value))}></input>
+            </div>
+            <div className='col-md-6'>
+              <label className='form-label'>Число 2</label>
+              <input
+              type='number'
+              className='form-control'
+              value={num2}
+              onChange={(e) => setNum2(Number(e.target.value))}></input>
+            </div>
+
+          </div>
+          <div className='d-flex gap-2 mb-2'>
+            <button
+            className='btn btn-success flex-fill'
+            onClick={() => setresult(String(plus(num1, num2)))}>+</button>
+             <button
+            className='btn btn-success flex-fill'
+            onClick={() => setresult(String(minus(num1, num2)))}>-</button>
+             <button
+            className='btn btn-success flex-fill'
+            onClick={() => setresult(String(division(num1, num2)))}>/</button>
+             <button
+            className='btn btn-success flex-fill'
+            onClick={() => setresult(String(multiply(num1, num2)))}>*</button>
+          </div>
+          <div className='mb-2'>
+            <label className='form-label mb-1'>Результат</label>
+            <input
+            type='text'
+            className='form-control'
+            value={result}
+            readOnly
+            placeholder='Тут будет результат'></input>
+          </div>
+        </div>
+      </div>
       
     </div>
   )
